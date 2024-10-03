ce47e175aeaeae95feedfc06a257883059e6f940
jin024144 <jin024144@gmail.com>
2024-10-04 05:35:57 +0900

    Update 18429.py

--- jin024144/18429.py
+++ jin024144/18429.py
@@ -1,6 +1,17 @@
 N,K=map(int,input().split()) 
 
-list = int(input().split())
+lst = list(map(int, input().split()))
+lst = [k-4 for k in lst]
+l3=[[] for _ in range(N+1)]
+l3[0]=lst
+for j in range(N):
+    for i in l3[j]:
+        if i >= 0:
+            l3[j+1].append(i)
+    l3[j+1]=[k-4 for k in l3[j+1]]
 
-print(list)
+
+
+
+print(l3)
 
